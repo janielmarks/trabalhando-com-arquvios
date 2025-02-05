b532553d537e937dd4e40e116b3d5d4f99487bb8
Janiel Marques <janielmarques@icloud.com>
2025-02-05 15:35:09 -0300

    FileReader e BufferedReader

---
+++ src/Application/Program.java
@@ -0,0 +1,47 @@
+package Application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+    public static void main(String[] args) {
+
+        String path = "/home/janiel-marques/Downloads/Casas Renome.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+
+            }
+        }
+        catch (
+                IOException e) {
+            System.out.println("Erroe: " + e.getMessage());
+        }
+        finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null){
+                    fr.close();
+                }
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
+
+
