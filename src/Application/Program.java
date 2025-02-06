355169b5aa726b2a150418c0c32c658e81edbc2e
Janiel Marques <janielmarques@icloud.com>
2025-02-05 21:34:58 -0300

    Manipulando pastas com File

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,23 +1,33 @@
 package Application;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
 
-        String[] lines = new String[] { "Good morning, afternoon, night"};
-        String path = "/home/janiel-marques/Downloads/teste.txt";
+        System.out.println("Enter a folder path: ");
+        String strPath = sc.nextLine();
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))){
-            for (String line: lines) {
-                bw.write(line);
-                bw.newLine();
-            }
+        File path = new File(strPath);
+        File[] folders = path.listFiles(File::isDirectory);
+        System.out.println("FOLDERS:");
+        for (File folder : folders) {
+            System.out.println(folder);
         }
-        catch (IOException e){
-            e.printStackTrace();
+        File[] files = path.listFiles(File::isFile);
+        System.out.println("FILES:");
+        for (File file: files) {
+            System.out.println(file);
+        }
+        boolean success = new File(strPath + "\\subdir").mkdir();
+        System.out.println("Directory created with successfully: " + success);
+
+        sc.close();
+
         }
     }
-}
+
 
 
